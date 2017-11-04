a59bb812ba9ac3f0776f94cd151d137ffe458c9a
waylulu <zhululu@hahahaha.local>
2017-11-04 09:38:46 +0800

    Initial Commit

---
+++ TestWebViewJS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestWebViewJS
+//
+//  Created by 朱路路 on 2017/11/4.
+//  Copyright © 2017年 huangteng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
